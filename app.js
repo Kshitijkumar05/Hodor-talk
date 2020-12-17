4c20343253ba69416054048a45de8336928a2ee9
Kshitijkumar05 <Kshitijkumar05@users.noreply.github.com>
2020-12-17 14:25:55 +0530

    done

--- app.js
+++ app.js
@@ -1,4 +1,31 @@
-var username=prompt("Enter your name");
+var btnTranslate=document.querySelector("#btn-translate");
+var txtInput=document.querySelector("#txt-input");
+var outputDiv=document.querySelector("#output");
 
-var message="Welcome "+username;
-alert(message);
+var serverURL = "https://api.funtranslations.com/translate/valyrian.json"
+
+function getTranslationURL(input){
+    return serverURL + "?" +"text=" + input
+}
+
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("something wrong with server! Try again after sometime")
+}
+
+
+
+function clickHandler(){
+    var input = txtInput.value; // taking input
+
+    fetch(getTranslationURL(input))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; //output
+        })
+        .catch(errorHandler)
+};
+
+
+btnTranslate.addEventListener("click", clickHandler)
